8167d38a8d69c3b0032c1b8a32b0ef30a8edfa4f
Leandro Arturi <lea.arturi@gmail.com>
2024-05-04 14:46:46 -0300

    AuthLogin and AuthRegister DTOs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,20 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @MessagePattern({ cmd: 'auth.register.user' })
-  registerUser() {
-    return 'registerUser';
+  registerUser(@Payload() registerUserDto: RegisterUserDto) {
+    return registerUserDto;
   }
 
   @MessagePattern({ cmd: 'auth.login.user' })
-  loginUser() {
-    return 'loginUser';
+  loginUser(@Payload() loginUserDto: LoginUserDto) {
+    return loginUserDto;
   }
 
   @MessagePattern({ cmd: 'auth.verify.user' })
